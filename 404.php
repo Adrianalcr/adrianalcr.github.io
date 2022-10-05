374e8a5fe7b66dbbc3a18358a1c480cf67a73b6b
Adriana Lima <83471694+Adrianalcr@users.noreply.github.com>
2022-10-05 10:02:38 -0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,6 +1,32 @@
-<?php
-include_once 'header.php';
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Portfólio profissional, especialista em tecnologia da informação.">
+    <meta name="author" content="Adriana Lima">
+    <link rel="shortcut icon" href="assets/images/logo-4.png" type="image/x-ico">
+
+    
+    <!-- Styles CSS -->
+    <link rel="stylesheet" href="assets/css/index.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
+
+    <!-- Fonts CSS -->
+    <link href="assets/fonts/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+        
+    <title> Adriana Lima Dev Full Stack</title>
+
+</head>
+
+<body>
 
 <!-- 404 Start -->
 <div class="err404 our-courses">
@@ -21,4 +47,5 @@ include_once 'header.php';
 <!-- 404 End -->
 
 <!-- Footer Start -->
-<?php include_once 'footer.php'; ?>
+  </body>
+</html>
